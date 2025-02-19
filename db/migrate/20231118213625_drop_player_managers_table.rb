9c26200d2f8a0303a3f2176955407529d6194e8d
Rithik <rithik.chdry@gmail.com>
2025-02-19 00:16:16 -0500

    pending migrations

--- db/migrate/20231118213625_drop_player_managers_table.rb
+++ db/migrate/20231118213625_drop_player_managers_table.rb
@@ -1,5 +1,5 @@
 class DropPlayerManagersTable < ActiveRecord::Migration[7.0]
   def change
-    drop_table :player_managers
+    drop_table :player_managers, if_exists: true
   end
 end
--- db/migrate/20231118213654_drop_players_table.rb
+++ db/migrate/20231118213654_drop_players_table.rb
@@ -1,5 +1,5 @@
 class DropPlayersTable < ActiveRecord::Migration[7.0]
   def change
-    drop_table :players
+    drop_table :players, if_exists: true
   end
 end
